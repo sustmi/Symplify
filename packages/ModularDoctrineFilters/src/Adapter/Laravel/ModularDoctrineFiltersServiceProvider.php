b259a16db4844feb1e3d4238c0aba1d850ec4ede
TomasVotruba <tomas.vot@gmail.com>
2017-03-12 21:46:21 +0100

    [ModularDoctrineFilters] decouple service filtering to PackageBuilder

--- packages/ModularDoctrineFilters/src/Adapter/Laravel/ModularDoctrineFiltersServiceProvider.php
+++ packages/ModularDoctrineFilters/src/Adapter/Laravel/ModularDoctrineFiltersServiceProvider.php
@@ -5,12 +5,10 @@ namespace Symplify\ModularDoctrineFilters\Adapter\Laravel;
 use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
-use ReflectionFunction;
-use Symfony\Component\DependencyInjection\Definition;
 use Symplify\ModularDoctrineFilters\Contract\Filter\FilterInterface;
 use Symplify\ModularDoctrineFilters\Contract\FilterManagerInterface;
 use Symplify\ModularDoctrineFilters\FilterManager;
+use Symplify\PackageBuilder\Adapter\Laravel\Container\DefinitionFinder;
 
 /**
  * @property Application $app
@@ -28,65 +26,10 @@ final class ModularDoctrineFiltersServiceProvider extends ServiceProvider
 
     public function boot(FilterManagerInterface $filterManager): void
     {
-        foreach ($this->app->getBindings() as $name => $definition) {
-            // todo: skip later...
-            // todo: detect if implements Interface before creating?
-            // use some class map like in Symfony?
-            // https://laravel.com/docs/5.4/container#container-events
+        $filterDefinitions = DefinitionFinder::findAllByType($this->app->getBindings(), FilterInterface::class);
 
-            if ($this->isDefinitionOfType($definition, FilterInterface::class)) {
-                $filterManager->addFilter($name, $this->app->make($name));
-            }
+        foreach ($filterDefinitions as $name => $definition) {
+            $filterManager->addFilter($name, $this->app->make($name));
         }
-    }
-
-    /**
-     * @param Definition[] $definition
-     * @param string $classOrInterfaceType
-     */
-    private function isDefinitionOfType(array $definition, string $classOrInterfaceType): bool
-    {
-        $closureReflection = new ReflectionFunction($definition['concrete']);
-
-        if ($this->isLaravelSystem($closureReflection)) {
-            return false;
-        }
-
-        $staticVariables = $closureReflection->getStaticVariables();
-
-        if ($this->hasVariableOfNameAndType($staticVariables, 'abstract', $classOrInterfaceType)) {
-            return true;
-        }
-
-        if ($this->hasVariableOfNameAndType($staticVariables, 'variable', $classOrInterfaceType)) {
-            return true;
-        }
-
-        // closure explicit return type
-        if ((string) $closureReflection->getReturnType() === $classOrInterfaceType) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function isLaravelSystem(ReflectionFunction $closureReflection): bool
-    {
-        return Str::startsWith($closureReflection->name, 'Illuminate') &&
-            Str::startsWith($closureReflection->name, 'Illuminate\\Container') === false;
-    }
-
-    /**
-     * @param string[] $staticVariables
-     * @param string $name
-     * @param string $classOrInterfaceType
-     */
-    private function hasVariableOfNameAndType(array $staticVariables, string $name, string $classOrInterfaceType): bool
-    {
-        if (! isset($staticVariables[$name])) {
-            return false;
-        }
-
-        return is_a($staticVariables[$name], $classOrInterfaceType, true);
     }
 }
